c79e5083f3baad7d804ed2d232ecfd78841dce14
agvan <agvandyke25@gmail.com>
2022-09-08 18:08:52 -0500

    created contact form in the Contact component

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 function Contact() {
     return (
-
+<section>
+    <h1>Contact Me</h1>
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor='name'>Name:</label>
+            <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
+        </div>
+        <div>
+            <label htmlFor='email'>Email address:</label>
+            <input type="email" name="email" defaultValue={email} onBlur={handleSubmit} />
+        </div>
+        {errorMessage && (
+            <div>
+                <p className='error-text'>{errorMessage}</p>
+            </div>
+        )}
+        <button type="submit">Submit</button>
+    </form>
+</section> 
     );
 }
 
