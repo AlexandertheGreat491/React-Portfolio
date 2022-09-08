c25ca757399867ea183e906ab717bd28492fcf39
agvan <agvandyke25@gmail.com>
2022-09-08 11:50:47 -0500

    set up nav element, addded div element with <a> element for About Me

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
-<header className='container col align-self-center col-sm col-md col-lg col-xl'>
-    <h1 className='text-left d-flex flex-row'>Alex Van Dyke</h1>
+  <header className="container col align-self-center col-sm col-md col-lg col-xl">
+    <h1 className="text-left d-flex flex-row">Alex Van Dyke</h1>
     {/* navigation */}
-</header>
-};
+    <nav className="nav nav-pills flex-column flex-sm-row">
+      <div className="col-sm">
+        <a
+          href="#About-Me"
+          className="flex-sm-fill text-sm-center nav-link m-3"
+        >
+          About Me
+        </a>
+      </div>
+    </nav>
+  </header>;
+}
 
 export default Header;
