703176adbfab962e3ae011fae4c8608607c858e1
agvan <agvandyke25@gmail.com>
2022-09-08 14:24:45 -0500

    added second of six projects to Portfolio component

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -2,12 +2,14 @@
 import React from "react";
 // imports the image for the Movie Nerds app
 import movieNerds from "../../assets/images/Movie_Nerds.jpg";
+// imports the image for the Julia's Children app
+import juliasChildren from "../../assets/images/Julias_Children_Screenshot_updated.jpg";
 
 function Portfolio() {
-    // six projects
+  // six projects
   return (
     <article className="container">
-        {/* Movie Nerds */}
+      {/* Movie Nerds */}
       <div className="row">
         <div>
           <div className="row d-inline-flex">
@@ -18,10 +20,27 @@ function Portfolio() {
               <a href="https://alexanderthegreat491.github.io/solid-octo-funicular">
                 <img src={movieNerds} style={{ width: "20%" }} alt="nerds" />
               </a>
+              <a href="https://github.com/AlexandertheGreat491/solid-octo-funicular.git">
+                Movie Nerds Repository
+              </a>
             </div>
           </div>
           {/* Julias Children */}
-
+          <div className="row d-inline-flex">
+            <div className="card border border-dark border-3 rounded mb-3 align-items-center col">
+              <h2 className="card-header bg-dark text-light d-flex-inline">
+                Julia's Children
+              </h2>
+              <a href="https://arcane-brushlands-08991.herokuapp.com/">
+                <img
+                  src={juliasChildren}
+                  style={{ width: "20%" }}
+                  alt="julia"
+                />
+              </a>
+              <a href="https://github.com/AlexandertheGreat491/Julias_Children.git">Julia's Children Repository</a>
+            </div>
+          </div>
         </div>
       </div>
     </article>
