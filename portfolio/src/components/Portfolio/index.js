6d3bbbe3f41332df31dd8f819c17b56460541496
agvan <agvandyke25@gmail.com>
2022-09-08 14:13:21 -0500

    added one project card and three comments to the Portfolio component

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+// imports react
+import React from "react";
+// imports the image for the Movie Nerds app
+import movieNerds from "../../assets/images/Movie_Nerds.jpg";
 
-function About() {
-    return (
+function Portfolio() {
+    // six projects
+  return (
+    <article className="container">
+        {/* Movie Nerds */}
+      <div className="row">
+        <div>
+          <div className="row d-inline-flex">
+            <div className="card border border-dark border-3 rounded mb-3 col align-items-center">
+              <h2 className="card-header bg-dark text-light d-flex-inline">
+                Movie Nerds
+              </h2>
+              <a href="https://alexanderthegreat491.github.io/solid-octo-funicular">
+                <img src={movieNerds} style={{ width: "20%" }} alt="nerds" />
+              </a>
+            </div>
+          </div>
+          {/* Julias Children */}
 
-    );
+        </div>
+      </div>
+    </article>
+  );
 }
+
+export default Portfolio;
