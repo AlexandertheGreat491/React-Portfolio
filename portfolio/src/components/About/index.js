5917080734b2f8f93644b0bbb2f6b0fe6334d922
agvan <agvandyke25@gmail.com>
2022-09-09 17:51:54 -0500

    added more information to the bio

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -13,15 +13,15 @@ function About() {
           <img src={profileImage} style={{ width: "20%" }} alt="profile" />
           <div className="col mt-4 border border-light border-4 rounded">
             <p id="bio" className="p-1 m-2">
-              A new developer leveraging a background in education, military
+              I am a new developer leveraging a background in education, military
               intelligence, and finance to provide unique perspectives on how
-              people interact with technology and in providing support to
-              customers. Innovative problem-solver who is passionate about
+              people interact with technology, providing support to
+              customers, and working collaboratively with team members. I consider myself to be an innovative problem-solver who is passionate about
               developing apps with a focus on mobile-first design and
               development and is currently completing a full-stack web
               development bootcamp through the University of Minnesota.
-              Strengths in creativity, teamwork, and building projects, and
-              customer service.
+              My strengths are in creativity, teamwork, building projects,
+              customer service, and research as a result of spending four years in Naval intelligence, working collaboratively on a team and witnessing how people of all ages interact with technology while teaching in the classroom, and honing my research skills in my post-secondary education and in my learning of new coding languages.
             </p>
           </div>
         </div>
