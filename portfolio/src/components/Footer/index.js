b12223069c5913ff9d102c393a9db8cdff553e5d
agvan <agvandyke25@gmail.com>
2022-09-08 20:43:48 -0500

    created the Footer component

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section>
+      <a href="https://github.com/AlexandertheGreat491">GitHub</a>
+      <a href="https://www.linkedin.com/in/alexander-van-dyke-a263a631">LinkedIn</a>
+      <a href="https://stackoverflow.com/users/18638174/alexander-van-dyke">Stack Overflow</a>
+    </section>
+  );
+}
+
+export default Footer;
