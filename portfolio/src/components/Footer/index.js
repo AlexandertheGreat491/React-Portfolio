5550d04d550bb1c9a7a59331cc871e87fdb76e31
agvan <agvandyke25@gmail.com>
2022-09-09 17:18:55 -0500

    footer now centered & every page takes up the whole window in the browser

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <footer id="foot">
-      <a id="foot" href="https://github.com/AlexandertheGreat491">GitHub</a>
-      <a id="foot" href="https://www.linkedin.com/in/alexander-van-dyke-a263a631">LinkedIn</a>
-      <a id="foot" href="https://stackoverflow.com/users/18638174/alexander-van-dyke">Stack Overflow</a>
-      <span>&copy; 2022 Alex Van Dyke</span>
+      <a id="foot" className='m-4'href="https://github.com/AlexandertheGreat491">GitHub</a>
+      <a id="foot" className='m-4' href="https://www.linkedin.com/in/alexander-van-dyke-a263a631">LinkedIn</a>
+      <a id="foot" className='m-4' href="https://stackoverflow.com/users/18638174/alexander-van-dyke">Stack Overflow</a>
+      <span className='m-4'>&copy; 2022 Alex Van Dyke</span>
     </footer>
   );
 }
