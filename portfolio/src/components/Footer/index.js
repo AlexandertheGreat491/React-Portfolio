ab8778f2a990c6a0a44a119a32638c896a623652
agvan <agvandyke25@gmail.com>
2022-09-11 11:05:30 -0500

    updated footer styling

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
       <a id="foot" className='m-4'href="https://github.com/AlexandertheGreat491">GitHub</a>
       <a id="foot" className='m-4' href="https://www.linkedin.com/in/alexander-van-dyke-a263a631">LinkedIn</a>
       <a id="foot" className='m-4' href="https://stackoverflow.com/users/18638174/alexander-van-dyke">Stack Overflow</a>
-      <span className='m-4'>&copy; 2022 Alex Van Dyke</span>
+      <span id="foot" className='m-4'>&copy; 2022 Alex Van Dyke</span>
     </footer>
   );
 }
