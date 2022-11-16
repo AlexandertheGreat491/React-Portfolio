929ce09a5880bbf83cffdf15e8814183cbc8df99
agvan <agvandyke25@gmail.com>
2022-11-16 15:35:52 -0600

    added background image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,42 +6,42 @@ import profileImage from "../../assets/images/profile picture.JPG";
 
 // imports the MERN logo
 import mernLogo from "../../assets/images/MERN-LOGO.jpg";
-import cityImage from "../../assets/images/City.jpg";
+// imports the background image
+import cityImage from "../../assets/images/City2.jpg";
 // reference https://www.inc.com/logan-chierotti/the-future-of-transportation-is-straight-outta-science-fiction.html
-// imports the asterisk icon
-import { FaAsterisk } from "react-icons/fa";
 
 // About-Me section of the portfolio
 // reference https://commons.wikimedia.org/wiki/File:MERN-logo.png
 
 function About() {
   return (
-    <article id="about" className="container mt-3" style={{backgroundImage:`url(${cityImage})`}}>
-      <section className="my-2">
-        <div className="col m-2">
+    <article id="about" className="container">
+      <section>
+        <div
+          id="top"
+          className="col"
+          style={{
+            backgroundImage: `url(${cityImage})`,
+            // backgroundSize: "cover",
+            backgroudRepeat: "no-repeat",
+            marginTop: "10px",
+            height: "100%",
+            margin: "10px"
+          }}
+        >
           <h2 id="me" className="mb-5">
             About Me
           </h2>
-          <h1 id="stackheader">What's in my stack?</h1>
-          <img
-            id="stack"
-            src={mernLogo}
-            alt="stack"
-            style={{ width: "20%" }}
-            className="mt-1 mb-2"
-          />
           <img
             id="handsome"
             src={profileImage}
             style={{ width: "20%" }}
             alt="profile"
+            className="ms-4"
           />
-          <div
-            className="col"
-            style={{ position: "relative", top: "-10.9375em" }}
-          >
+          <div className="col" style={{ position: "relative", top: "-40px" }}>
             <h1 id="bioheader">Who am I?</h1>
-            <p id="bio" className="mt-4 ms-3">
+            <p id="bio" className="mt-5">
               I am a new developer leveraging a background in education,
               military intelligence, and finance to provide unique perspectives
               on how people interact with technology, providing support to
@@ -50,16 +50,14 @@ function About() {
               passionate about developing apps with a focus on mobile-first
               design and development and is currently completing a full-stack
               web development bootcamp through the University of Minnesota.
-              <FaAsterisk id="bioicon" size={15} className="mt-4"/>  
-            </p>
-            
-          </div>
-          
-          <div
-            className="col"
-            style={{ position: "relative", top: "-10.9375em" }}
-          >
-            <p id="bio" className="mt-4 ms-3">
+              <br></br>
+              <img
+                id="stack"
+                src={mernLogo}
+                alt="stack"
+                style={{ width: "20%" }}
+                className="mt-1 mb-2"
+              />
               My strengths are in creativity, teamwork, building projects,
               customer service, and research as a result of spending four years
               in Naval intelligence, working collaboratively on a team and
