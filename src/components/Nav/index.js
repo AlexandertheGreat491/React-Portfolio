cb6de2d932abd833e8af2070a5330d56b880ef40
agvan <agvandyke25@gmail.com>
2023-03-04 14:00:49 -0600

    changed button width, margin-left, and made nav w-100

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,11 +8,11 @@ function Nav(props) {
   } = props;
 
   return (
-  <nav id='navigation' className='mt-3'>
+  <nav id='navigation' className='container justify-content-center d-inline-flex flex-row mt-3 w-100'>
     {categories.map((category) => (
                 <button id='nav-item'  key={category.name}
                     className={`${currentCategory.name === category.name && 'navActive'
-                       }`}>
+                       }`} style={{width:"20%", height: "15%"}}>
                     <span onClick={() => setCurrentCategory(category)}>
                         {category.name}
                     </span>
