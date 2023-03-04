6f01831ab5969203888c0f77678b869c12d8a048
agvan <agvandyke25@gmail.com>
2023-03-04 13:27:21 -0600

    added several comments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function Header() {
     return (
-        <header className='container col align-self-center col-sm col-md col-lg col-xl'>
+        <header className='container col align-self-center col-sm col-md col-lg col-xl d-inline-flex pb-3'>
+            <div>
             <h1 className='text-left d-flex flex-row'>Alex Van Dyke</h1>
+            </div>
+            
         </header>
     );
 }
