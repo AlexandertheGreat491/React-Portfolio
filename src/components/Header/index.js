cd84adad35e94fd1fea7fa1f0a5c04257f280684
agvan <agvandyke25@gmail.com>
2023-03-04 13:47:07 -0600

    changed font-size and font-weight of the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="col-12 d-inline-flex pb-2">
       <div>
-        <h1 className="d-inline-flex w-100 justify-cotent-center ms-4">Alex Van Dyke</h1>
+        <h1 id="navheader" className="d-inline-flex w-100 justify-cotent-center ms-4">Alex Van Dyke</h1>
       </div>
     </header>
   );
