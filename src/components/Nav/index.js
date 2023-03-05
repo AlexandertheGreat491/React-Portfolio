ef627f490fc65d48bbe4b312412567d9b6fc9426
agvan <agvandyke25@gmail.com>
2023-03-04 19:51:46 -0600

    added angle icon

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaAngleRight } from 'react-icons/fa';
 
 function Nav(props) {
   const {
@@ -9,15 +10,19 @@ function Nav(props) {
 
   return (
   <nav id='navigation' className='container justify-content-center d-inline-flex flex-row mt-5 w-100'>
+     
     {categories.map((category) => (
-                <button id='nav-item'  key={category.name}
+     
+                <button id='nav-item'   key={category.name}
                     className={`${currentCategory.name === category.name && 'navActive'
-                       }`} style={{width:"20%", height: "15%"}}>
+                       }`} style={{width:"20%", height: "15%"}} >
+                        <FaAngleRight id='angle'/>
                     <span onClick={() => setCurrentCategory(category)}>
                         {category.name}
                     </span>
-                </button>
+                </button> 
             ))}
+            
   </nav>
   )
 } 
