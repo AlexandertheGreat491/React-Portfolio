bf69f035b0a5673fcd41d83a04dc1c8f74669d59
agvan <agvandyke25@gmail.com>
2023-03-17 20:15:09 -0500

    added one comment

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 
 // imports the icons
+// imports the Github icon
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaStackOverflow } from "react-icons/fa";
