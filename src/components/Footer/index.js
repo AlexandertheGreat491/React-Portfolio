8ee707dcc70244f2a287c4bb298d45b8ce9400d7
agvan <agvandyke25@gmail.com>
2023-03-17 20:09:47 -0500

    changed year in copyright in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer() {
       <a id="foot" className='m-4'href="https://github.com/AlexandertheGreat491"><FaGithub className="me-1"/>GitHub</a>
       <a id="foot" className='m-4' href="https://www.linkedin.com/in/alexander-van-dyke-a263a631"><FaLinkedin className="me-1"/>LinkedIn</a>
       <a id="foot" className='m-4' href="https://stackoverflow.com/users/18638174/alexander-van-dyke"><FaStackOverflow className="me-1"/>Stack Overflow</a>
-      <span id="foot" className='m-4'>&copy; 2022 Alex Van Dyke</span>
+      <span id="foot" className='m-4'>&copy; 2023 Alex Van Dyke</span>
     </footer>
   );
 }
