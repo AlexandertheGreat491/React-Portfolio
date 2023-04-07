3dee1150fad4e6118ecf4cf24e1da2f8ccb58b3d
agvan <agvandyke25@gmail.com>
2023-04-07 13:53:14 -0500

    added display and flex properties to nav buttons

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from "react";
 // imports the right angle arrow icon
-import { FaAngleRight } from 'react-icons/fa';
+import { FaAngleRight } from "react-icons/fa";
 
 function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    currentCategory
-  } = props;
+  const { categories = [], setCurrentCategory, currentCategory } = props;
 
   return (
-  <nav id='navigation' className='d-inline-flex justify-content-center navbar navbar-expand{-sm|-md|-lg|-xl|-xxl} container-fluid m-2 position-relative'>
-     
-    {categories.map((category) => (
-     
-                <button id='nav-item'   key={category.name}
-                    className={`${currentCategory.name === category.name && 'navActive'
-                       }`} style={{width:"15%", height: "15%"}} >
-                        <FaAngleRight id='angle' className='m-1 position-relative d-inline-flex'/>
-                    <span onClick={() => setCurrentCategory(category)}>
-                        {category.name}
-                    </span>
-                </button> 
-            ))}
-            
-  </nav>
-  )
-} 
+    <nav
+      id="navigation"
+      className="d-inline-flex justify-content-center navbar navbar-expand{-sm|-md|-lg|-xl|-xxl} container-fluid m-2 position-relative"
+    >
+      {categories.map((category) => (
+        <button
+          id="nav-item"
+          key={category.name}
+          className={`${currentCategory.name === category.name && "navActive"}`}
+          style={{ width: "12%", height: "12%" }}
+        >
+          <FaAngleRight
+            id="angle"
+            className="m-1 position-relative d-inline-flex align-items-center"
+          />
+          <span onClick={() => setCurrentCategory(category)}>
+            {category.name}
+          </span>
+        </button>
+      ))}
+    </nav>
+  );
+}
 
 export default Nav;
