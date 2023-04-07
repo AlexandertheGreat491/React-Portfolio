d8cebbea20d3f07e5d27d42e2cf4daf4f8110f0e
agvan <agvandyke25@gmail.com>
2023-04-07 13:44:38 -0500

    attempting to make nav responsive

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,18 +10,18 @@ function Nav(props) {
   } = props;
 
   return (
-  <nav id='navigation' className='container justify-content-center d-inline-flex flex-row mt-5 w-100'>
+  <nav id='navigation' className='d-inline-flex justify-content-center navbar navbar-expand{-sm|-md|-lg|-xl|-xxl} container-fluid m-2 position-relative'>
      
     {categories.map((category) => (
      
-                <button id='nav-item'   key={category.name}
+                <li id='nav-item'   key={category.name}
                     className={`${currentCategory.name === category.name && 'navActive'
-                       }`} style={{width:"20%", height: "15%"}} >
-                        <FaAngleRight id='angle' className='m-1'/>
+                       }`} style={{width:"15%", height: "15%"}} >
+                        <FaAngleRight id='angle' className='m-1 position-relative d-inline-flex'/>
                     <span onClick={() => setCurrentCategory(category)}>
                         {category.name}
                     </span>
-                </button> 
+                </li> 
             ))}
             
   </nav>
