44e07bb0bc8c11ab123b2c43802bb99413d9354d
agvan <agvandyke25@gmail.com>
2023-04-07 15:09:31 -0500

    restyled the Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,21 +1,25 @@
 import React from "react";
+
 // imports the right angle arrow icon
 import { FaAngleRight } from "react-icons/fa";
+
 
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props;
 
   return (
+    
     <nav
       id="navigation"
       className="d-inline-flex justify-content-center navbar dropdown navbar-expand{-sm|-md|-lg|-xl|-xxl} container-fluid m-2 position-relative"
+      style={{width:"60%", height: "25%"}}
     >
       {categories.map((category) => (
-        <button
+        <li
           id="nav-item"
           key={category.name}
-          className={`${currentCategory.name === category.name && "navActive" && "d-inline-flex" && "align-items-center" && "position-relative"}`}
-          style={{ width: "12%", height: "12%" }}
+          className={`${currentCategory.name === category.name && "navActive" && "d-inline-block" && "align-items-center" && "position-relative" && "m-2"}`}
+          style={{ width: "15%", height: "15%" }}
         >
           <FaAngleRight
             id="angle"
@@ -24,7 +28,7 @@ function Nav(props) {
           <span className="d-inline-flex" onClick={() => setCurrentCategory(category)}>
             {category.name}
           </span>
-        </button>
+        </li>
       ))}
     </nav>
   );
